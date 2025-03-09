be79d1d80387f4de9aa738a4373c6c4968085749
JL <jlblaz.tim@gmail.com>
2025-03-09 16:24:14 +0100

    Arreglo paso de home a productos, se ha añadido componente supemercados, queda pendiente de mejorar las 3 interfaces con su conexión a la bbdd

--- PCII_GRUPO5/src/app/app.routes.ts
+++ PCII_GRUPO5/src/app/app.routes.ts
@@ -1,18 +1,11 @@
-// app.routes.ts
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductosComponent } from './productos/productos.component';
+import { SupermercadosComponent } from './supermercados/supermercados.component';
 
 export const routes: Routes = [
-  // Ruta padre: 'home'
-  {
-    path: 'home',
-    component: HomeComponent,
-    children: [
-      // Ruta hija: 'home/productos'
-      { path: 'productos', component: ProductosComponent },
-      // Aquí puedes añadir más rutas hijas
-    ]
-  },
+  { path: 'home', component: HomeComponent },
+  { path: 'productos', component: ProductosComponent }, 
+  { path: 'supermercado', component: SupermercadosComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
--- PCII_GRUPO5/src/app/productos/productos.component.ts
+++ PCII_GRUPO5/src/app/productos/productos.component.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-productos',
-  imports: [],
   templateUrl: './productos.component.html',
-  styleUrl: './productos.component.scss'
+  styleUrls: ['./productos.component.scss']
 })
 export class ProductosComponent {
-
+  
 }

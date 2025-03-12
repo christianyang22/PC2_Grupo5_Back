bc27e51423cd8b9fa13a74c4f5e5ced1af1e30b4
marto30 <61737017+marto30@users.noreply.github.com>
2025-03-12 13:48:46 +0100

    perfil-test

--- PCII_GRUPO5/src/app/app.routes.ts
+++ PCII_GRUPO5/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductosComponent } from './productos/productos.component';
 import { SupermercadosComponent } from './supermercados/supermercados.component';
+import { PerfilComponent } from './perfil/perfil.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'productos', component: ProductosComponent }, 
   { path: 'supermercados', component: SupermercadosComponent },
+  { path: 'perfil', component: PerfilComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];

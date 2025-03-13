7a6234e5c58c930b58c5700405df83c0901dbdec
GEORGEEEEEEEEEEEEE <04js07@gmail.com>
2025-03-13 12:16:56 +0100

    Corrección Rutas

--- PCII_GRUPO5/src/app/app.routes.ts
+++ PCII_GRUPO5/src/app/app.routes.ts
@@ -2,12 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductosComponent } from './productos/productos.component';
 import { SupermercadosComponent } from './supermercados/supermercados.component';
-import { PerfilComponent } from './perfil/perfil.component';
+
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'productos', component: ProductosComponent }, 
   { path: 'supermercados', component: SupermercadosComponent },
-  { path: 'perfil', component: PerfilComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
